80811cbfcc7c9359e1a23a0a00f4d127f5642df3
Kichinda34 <leandroschneiderlima@gmail.com>
2023-03-09 11:47:58 -0300

    Impressão corrigida, e anotada uma outra maneira de realizar  a mesma operação no código.

--- MatrizIrregular/Program.cs
+++ MatrizIrregular/Program.cs
@@ -7,12 +7,14 @@ internal class Program
     {
         double[,] matriz = new double[5, 3];
 
+        Console.WriteLine("Calculando...");
+        Console.WriteLine("Os valores da Matriz são: ");
         for (int l = 0; l < 5; l++)
         {
             for (int c = 0; c < 3; c++)
             {
                 Random n = new Random();
-                double b = n.NextDouble() * 100;
+                double b = (double) n.Next(1000)/100;
 
                 if (c == 0 || c == 1)
                 {
@@ -22,9 +24,40 @@ internal class Program
                 {
                     matriz[l, c] = (matriz[l, 0] + matriz[l, 1]) / 2;
                 }
-                Console.Write(matriz[l,c] + " | \n");
+                Console.Write("\tmatriz[" + l + "][" + c + "]" + matriz[l, c].ToString("f2"));
             }
+            Console.WriteLine();
+
         }
+
+
+        /*otimizado
+           for (int c = 0; c < matriz.GetLength(1)-1; c++)
+           {
+               for (int l = 0; l < matriz.GetLength(0); l++)
+               {
+                   matriz[l,c] = (double) b.Next(1000)/100;
+               }
+           }
+
+           //imprimindo a matriz
+
+               for (int l = 0; l < matriz.GetLength(0); l++)
+               {
+                   for (int c = 0; c < matriz.GetLength(1)-1; c++)
+                   {
+                       Console.Write("matriz[" + l + "][" + c + "]" + matriz[l,c]);
+                   }
+               }
+
+           //calculo 3º coluna
+
+           Console.WriteLine("Calculando...");
+           for (int l = 0; l < matriz.GetLength(0); l++)
+           {
+               matriz[l,2] = (matriz[l,0] + matriz[l,1]);
+           }
+           */
     }
 }
 
